2295d583efd07d87ff596e98e9da4cadbf063729
James Esper <eac-ph@live.com>
2024-08-23 22:49:25 +0700

    Add project files.

---
+++ CLS_CHART.vb
@@ -0,0 +1,144 @@
+﻿' This class is used to monitor: Now_node(,), Now_TotalTime, Now_CurrentTime, and Now_PV
+' It starts scanning by itself
+' Call Example:
+' Private Sub newChartLog()
+'     Dim CHRT As New CLS_CHART(Me)
+'     CHRT.Location = New Point(10, 10)
+'     CHRT.Width = 600
+'     CHRT.Height = 600
+'     Me.Controls.Add(CHRT)
+' End Sub
+' Class completed on 16 Aug 2024 by Esper.
+Imports System.Windows.Forms
+Imports System.Drawing
+Imports ScottPlot
+Imports ScottPlot.WinForms
+Imports ScottPlot.Colormaps
+Imports ScottPlot.Stylers
+
+Public Class CLS_CHART
+    Inherits Panel
+
+    ' ScottPlot control for plotting data
+    Private ReadOnly iChart As New FormsPlot
+
+    ' UI elements
+    Private WithEvents MainGroup As New System.Windows.Forms.GroupBox()
+    Private WithEvents mainTimer As New Timer
+    Private WithEvents StatusPanel As New Panel
+    Private WithEvents Lbl_Info As New System.Windows.Forms.Label
+
+    ' Reference to the main form
+    Private mainForm As Form1
+
+    ' Constructor to initialize the chart with the main form
+    Public Sub New(Form As Form1)
+        mainForm = Form ' Copy main form reference
+
+        ' Setup the main group box
+        MainGroup.Width = 600
+        MainGroup.Height = 260
+        MainGroup.Text = "Data log"
+        Me.Controls.Add(MainGroup)
+
+        ' Setup the ScottPlot control
+        iChart.Width = 550
+        iChart.Height = 230
+        iChart.Location = New Point(10, 30)
+        iChart.Plot.XLabel("Time (min)")
+        iChart.Plot.YLabel("Temperature (°C)")
+        iChart.BackColor = System.Drawing.Color.FromArgb(255, 255, 255)
+        MainGroup.Controls.Add(iChart)
+
+        ' Setup the status panel for blinking status indicator
+        StatusPanel.Location = New Point(585, 20)
+        StatusPanel.Width = 10
+        StatusPanel.Height = 10
+        StatusPanel.BackColor = System.Drawing.Color.White
+        MainGroup.Controls.Add(StatusPanel)
+
+        ' Setup the timer
+        mainTimer.Interval = 100
+        mainTimer.Start()
+
+        ' Setup the label for displaying information
+        Lbl_Info.Location = New Point(0, 300)
+        Lbl_Info.Width = 300
+        Lbl_Info.Height = 250
+        Me.Controls.Add(Lbl_Info)
+    End Sub
+
+    ' Variable to toggle status logic
+    Private StatusLogic As Boolean
+
+    ' Timer tick event to update the plot
+    Private Sub myTimer_Tick(sender As Object, e As EventArgs) Handles mainTimer.Tick
+        UpdatePlot()
+    End Sub
+
+    ' Initialize arrays for X and Y values
+    Private xValues(9) As Double
+    Private yValues(9) As Double
+
+    ' Declare lists to store timeline and PV on-time data
+    Private TimeLine As New List(Of Double)()
+    Private PVOntime As New List(Of Double)()
+
+    ' Method to update the plot with new data
+    Private Sub UpdatePlot()
+        ' Add data to the lists if the current time is less than the total time
+        If mainForm.MValues.Now_CurrentTime < mainForm.MValues.Now_TotalTime Then
+            TimeLine.Add(mainForm.MValues.Now_CurrentTime.TotalMinutes)
+            PVOntime.Add(mainForm.MValues.Get_PV)
+        End If
+
+        ' Convert lists to arrays
+        Dim TimeLineArray() As Double = TimeLine.ToArray()
+        Dim PVOntimeArray() As Double = PVOntime.ToArray()
+
+        ' Toggle status logic
+        StatusLogic = Not StatusLogic
+
+        ' Check if invoke is required for thread safety
+        If Me.InvokeRequired Then
+            Me.Invoke(New Action(AddressOf UpdatePlot))
+        Else
+            ' Clear the plot
+            iChart.Plot.Clear()
+            ValveHighlight()
+            ' Update label text and fill arrays with random values for demonstration
+            Lbl_Info.Text = ""
+            For i As Integer = 0 To 9
+                xValues(i) = mainForm.MValues.Now_node(0, i)
+                yValues(i) = (New Random()).Next(0, 100)
+                Lbl_Info.Text &= $"{xValues(i)}, {yValues(i)}{vbLf}"
+            Next
+
+            ' Add data to the plot
+            iChart.Plot.Add.Scatter(TimeLineArray, PVOntimeArray)
+            iChart.Plot.Add.Scatter(xValues, yValues)
+
+            ' Auto scale the axes
+            iChart.Plot.Axes.AutoScale()
+            iChart.Refresh()
+
+            ' Update status panel color
+            StatusPanel.BackColor = If(StatusLogic, System.Drawing.Color.Green, System.Drawing.Color.White)
+        End If
+    End Sub
+    Private Sub ValveHighlight()
+        Dim vSpan As New Plot
+        iChart.Plot.Add.HorizontalSpan(mainForm.MValues.OpenVA.TotalMinutes, mainForm.MValues.CloseVA.TotalMinutes)
+        iChart.Plot.Add.HorizontalSpan(mainForm.MValues.OpenVB.TotalMinutes, mainForm.MValues.CloseVB.TotalMinutes)
+        ' Refresh the plot to apply changes
+        iChart.Refresh()
+
+    End Sub
+
+    Private Sub SetFontForControl(ctrl As System.Windows.Forms.Control, fontName As String, fontSize As Single, fontStyle As System.Drawing.FontStyle)
+        Dim newFont As New System.Drawing.Font(fontName, fontSize, fontStyle)
+        ctrl.Font = newFont
+    End Sub
+
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar
+
+Public Class Form1
+
+    Public Structure MainValues
+        ' SET_VALUE
+        Public Set_SV As Double
+        Public Set_Door1 As Boolean
+        Public Set_Door2 As Boolean
+        Public Set_Crush1 As Boolean
+        Public Set_Crush2 As Boolean
+        Public Set_Val1 As Boolean
+        Public Set_Val2 As Boolean
+        ' GET_VALUE
+        Public Get_PV As Double
+        Public Get_DrPosUp As Integer
+        Public Get_DrPosDw As Integer
+        ' PROCESS_VALUE
+        Public Now_TotalTime As TimeSpan
+        Public Now_CurrentTime As TimeSpan
+        Public Now_node(,) As Double
+        Public OpenVA As TimeSpan
+        Public OpenVB As TimeSpan
+        Public CloseVA As TimeSpan
+        Public CloseVB As TimeSpan
+    End Structure
+    Public MValues As New MainValues()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetupTest()
+        newChartLog()
+    End Sub
+
+
+    '---------------------------------------------------------------------------------------
+#Region "Tester"
+    Private Sub SetupTest()
+        MValues.OpenVA = MValues.OpenVA.Add(TimeSpan.FromMinutes(20))
+        MValues.CloseVA = MValues.CloseVA.Add(TimeSpan.FromMinutes(200))
+        MValues.OpenVA = MValues.OpenVA.Add(TimeSpan.FromMinutes(20))
+        MValues.CloseVB = MValues.CloseVB.Add(TimeSpan.FromMinutes(290))
+
+        MValues.Now_node = New Double(1, 10) {} ' Initialize the 2D array
+        For x As Integer = 0 To 9
+            MValues.Now_node(0, x) = x * 2
+            MValues.Now_node(1, x) = x * x
+        Next
+        MValues.Now_TotalTime = MValues.Now_TotalTime.Add(TimeSpan.FromMinutes(300))
+        Timer1.Interval = 100 '' Interval 100mm as 1 minute
+        Timer1.Start()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If MValues.Now_CurrentTime > MValues.Now_TotalTime Then
+            Timer1.Stop()
+        End If
+        MValues.Now_CurrentTime = MValues.Now_CurrentTime.Add(TimeSpan.FromMinutes(1))
+        MValues.Get_PV = SmoothRandom(200, 900, 0.1)
+    End Sub
+
+    ' Declare the Random object
+    Private rand As New Random()
+    ' Function to generate a smooth random value
+    Private Function SmoothRandom(min As Integer, max As Integer, smoothingFactor As Double) As Double
+        ' Generate two random values
+        Dim value1 As Double = rand.Next(min, max)
+        Dim value2 As Double = rand.Next(min, max)
+
+        ' Interpolate between the two values
+        Return (1 - smoothingFactor) * value1 + smoothingFactor * value2
+    End Function
+#End Region
+    '---------------------------------------------------------------------------------------
+
+    Private Sub newChartLog()
+        Dim CHRT As New CLS_CHART(Me)
+        CHRT.Location = New Point(10, 10)
+        CHRT.Width = 600
+        CHRT.Height = 600
+        Me.Controls.Add(CHRT)
+    End Sub
+
+
+
+End Class
